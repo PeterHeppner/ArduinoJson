aae41996aeac8f3fdf8b52cfeb3fcb26a495bf7e
Benoit Blanchon <github@benoitblanchon.fr>
2018-04-11 12:00:57 +0200

    Remove references to DynamicJsonObject

--- test/JsonParser/nestingLimit.cpp
+++ test/JsonParser/nestingLimit.cpp
@@ -9,62 +9,24 @@
 #define SHOULD_FAIL(expression) REQUIRE(JsonError::TooDeep == expression);
 
 TEST_CASE("JsonParser nestingLimit") {
-  SECTION("deserializeJson(JsonArray&)") {
-    DynamicJsonArray arr;
+  DynamicJsonDocument doc;
 
-    SECTION("limit = 0") {
-      SHOULD_FAIL(deserializeJson(arr, "[]", 0));
-    }
-
-    SECTION("limit = 1") {
-      SHOULD_WORK(deserializeJson(arr, "[]", 1));
-      SHOULD_FAIL(deserializeJson(arr, "[[]]", 1));
-    }
-
-    SECTION("limit = 2") {
-      SHOULD_WORK(deserializeJson(arr, "[[]]", 2));
-      SHOULD_FAIL(deserializeJson(arr, "[[[]]]", 2));
-    }
+  SECTION("limit = 0") {
+    SHOULD_WORK(deserializeJson(doc, "\"toto\"", 0));
+    SHOULD_WORK(deserializeJson(doc, "123", 0));
+    SHOULD_WORK(deserializeJson(doc, "true", 0));
+    SHOULD_FAIL(deserializeJson(doc, "[]", 0));
+    SHOULD_FAIL(deserializeJson(doc, "{}", 0));
+    SHOULD_FAIL(deserializeJson(doc, "[\"toto\"]", 0));
+    SHOULD_FAIL(deserializeJson(doc, "{\"toto\":1}", 0));
   }
 
-  SECTION("deserializeJson(JsonObject&)") {
-    DynamicJsonObject obj;
-
-    SECTION("limit = 0") {
-      SHOULD_FAIL(deserializeJson(obj, "{}", 0));
-    }
-
-    SECTION("limit = 1") {
-      SHOULD_WORK(deserializeJson(obj, "{\"key\":42}", 1));
-      SHOULD_FAIL(deserializeJson(obj, "{\"key\":{\"key\":42}}", 1));
-    }
-
-    SECTION("limit = 2") {
-      SHOULD_WORK(deserializeJson(obj, "{\"key\":{\"key\":42}}", 2));
-      SHOULD_FAIL(deserializeJson(obj, "{\"key\":{\"key\":{\"key\":42}}}", 2));
-    }
-  }
-
-  SECTION("deserializeJson(JsonVariant&)") {
-    DynamicJsonDocument var;
-
-    SECTION("limit = 0") {
-      SHOULD_WORK(deserializeJson(var, "\"toto\"", 0));
-      SHOULD_WORK(deserializeJson(var, "123", 0));
-      SHOULD_WORK(deserializeJson(var, "true", 0));
-      SHOULD_FAIL(deserializeJson(var, "[]", 0));
-      SHOULD_FAIL(deserializeJson(var, "{}", 0));
-      SHOULD_FAIL(deserializeJson(var, "[\"toto\"]", 0));
-      SHOULD_FAIL(deserializeJson(var, "{\"toto\":1}", 0));
-    }
-
-    SECTION("limit = 1") {
-      SHOULD_WORK(deserializeJson(var, "[\"toto\"]", 1));
-      SHOULD_WORK(deserializeJson(var, "{\"toto\":1}", 1));
-      SHOULD_FAIL(deserializeJson(var, "{\"toto\":{}}", 1));
-      SHOULD_FAIL(deserializeJson(var, "{\"toto\":[]}", 1));
-      SHOULD_FAIL(deserializeJson(var, "[[\"toto\"]]", 1));
-      SHOULD_FAIL(deserializeJson(var, "[{\"toto\":1}]", 1));
-    }
+  SECTION("limit = 1") {
+    SHOULD_WORK(deserializeJson(doc, "[\"toto\"]", 1));
+    SHOULD_WORK(deserializeJson(doc, "{\"toto\":1}", 1));
+    SHOULD_FAIL(deserializeJson(doc, "{\"toto\":{}}", 1));
+    SHOULD_FAIL(deserializeJson(doc, "{\"toto\":[]}", 1));
+    SHOULD_FAIL(deserializeJson(doc, "[[\"toto\"]]", 1));
+    SHOULD_FAIL(deserializeJson(doc, "[{\"toto\":1}]", 1));
   }
 }
